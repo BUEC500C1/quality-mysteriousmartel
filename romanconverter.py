24e7c3d0c1aeb79010b6105511099b06bc13ded6
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2020-01-29 00:13:32 -0500

    Add files via upload

--- romanconverter.py
+++ romanconverter.py
@@ -12,6 +12,7 @@ def errorCheck(inputVal):
 		print("Invalid input")
 		return False
 
+
 def whatHaveTheRomans(piggies):
 	Romani = {1: 'I', 5: 'V', 10: 'X', 50: 'L', \
 	100: 'C', 500: 'D', 1000: 'M'}
@@ -24,27 +25,27 @@ def whatHaveTheRomans(piggies):
 				Domum.append(Romani[1000])
 				Eunt -= 1000
 			else:
-				if ((Eunt - 900) <= 100 and (Eunt - 900) >= 0):
+				if ((Eunt - 900) < 100 and (Eunt - 900) >= 0):
 					Domum.append(Romani[100])
 					Domum.append(Romani[1000])
 					Eunt -= 900
-				elif ((Eunt - 400) <= 100 and (Eunt - 400) >= 0):
+				elif ((Eunt - 400) < 100 and (Eunt - 400) >= 0):
 					Domum.append(Romani[100])
 					Domum.append(Romani[500])
 					Eunt -= 400
-				elif ((Eunt - 90) <= 10 and (Eunt - 90) >= 0):
+				elif ((Eunt - 90) < 10 and (Eunt - 90) >= 0):
 					Domum.append(Romani[10])
 					Domum.append(Romani[100])
 					Eunt -= 90
-				elif ((Eunt - 40) <= 10 and (Eunt - 40) >= 0):
+				elif ((Eunt - 40) < 10 and (Eunt - 40) >= 0):
 					Domum.append(Romani[10])
 					Domum.append(Romani[50])
 					Eunt -= 40
-				elif ((Eunt - 9) <= 1 and (Eunt - 9) >= 0):
+				elif ((Eunt - 9) < 1 and (Eunt - 9) >= 0):
 					Domum.append(Romani[1])
 					Domum.append(Romani[10])
 					Eunt -= 9
-				elif ((Eunt - 4) <= 1 and (Eunt - 4) >= 0):
+				elif ((Eunt - 4) < 1 and (Eunt - 4) >= 0):
 					Domum.append(Romani[1])
 					Domum.append(Romani[5])
 					Eunt -= 4
@@ -58,8 +59,8 @@ def whatHaveTheRomans(piggies):
 
 if __name__ == '__main__':
 
-	testerVal = '19'
-	testerTwo = 19
+	testerVal = '10*2'
+	testerTwo = 10.1
 
 	print(whatHaveTheRomans(testerVal))
 	print(whatHaveTheRomans(testerTwo))
--- test_romanconverter.py
+++ test_romanconverter.py
@@ -1,1 +1,21 @@
-#Hi, I am filler
+# Copyright 2020 by Jennifer Campbell
+
+from romanconverter import *
+
+checkLib = {1: '1', 2: 15, 3: '49', 4: '58', \
+5: '99', 6: 300, 7: '459', 8: '567', 9: '999', \
+10: '1110', 11: 3999, 12: 4001, 13: 10.1, \
+14: '-1', 15: '1a0', 16: '10*2'}
+
+ansLib = {1: 'I', 2: 'XV', 3: 'XLIX', 4: 'LVIII', \
+5: 'XCIX', 6: 'CCC', 7: 'CDLIX', 8: 'DLXVII', \
+9: 'CMXCIX', 10: 'MCX', 11: 'MMMCMXCIX', \
+12: 'MMMMI', 13: False, 14: False, 15: False, \
+16: False}
+
+
+
+if __name__ == '__main__':
+	i = 1
+	for i in range(len(checkLib)):
+		print(whatHaveTheRomans(checkLib[i]))
